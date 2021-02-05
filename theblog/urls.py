2de8ce3885feeabde4ea3b27497e78eb59b01add
Gautam jain <55816462+gautamj-tech@users.noreply.github.com>
2021-02-05 17:36:21 +0530

    Add files via upload

---
+++ theblog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView, AddCommentView,AddPrivate
+ 
+urlpatterns = [
+    #path('', views.home, name="home"),
+    path('', views.pp, name='home'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:cats>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category-list'),
+    path('like/<int:pk>', LikeView, name='like_post'),
+    path('article/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
+    path('add_private/', AddPrivate.as_view(), name='add_private'),
+    path('add_private/', AddPrivate.as_view(), name='add_private'),
+    path('pp/', HomeView.as_view(), name="pp"),
+
+
+]
